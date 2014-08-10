42fe42291adb1422d192bea738d4329657d03dae
Dave Wyatt <dlwyatt115@gmail.com>
2014-08-09 22:27:58 -0400

    Refactored test code to eliminate some duplication

--- StringTokens.Tests.ps1
+++ StringTokens.Tests.ps1
@@ -3,6 +3,19 @@ Remove-Module StringTokens -Force -ErrorAction SilentlyContinue
 $scriptRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 Import-Module $scriptRoot\StringTokens.psm1 -Force -ErrorAction Stop
 
+function Assert-ArraysAreEqual
+{
+    param ([object[]] $First, [object[]] $Second)
+    if ($null -eq $First)  { $First = @() }
+    if ($null -eq $Second) { $Second = @() }
+
+    $First.Count | Should Be $Second.Count
+    for ($i = 0; $i -lt $First.Count; $i++)
+    {
+        $First[$i] | Should BeExactly $Second[$i]
+    }
+}
+
 Describe 'Get-StringToken (public API)' {
     Context 'When using the default behavior and only passing in strings.' {
         It 'Uses only space and tab as delmiters within each line' {
@@ -27,14 +40,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', 'Two Three', "'Four'"
             $result = @(Get-StringToken -String $lines)
 
-            # TODO:  Change these separate assertions into a simpler $result | Should BeExactly $expected
-            # if / when PR#175 is merged into Pester.
-
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Produces empty tokens when multiple consecutive delimiters are found' {
@@ -42,11 +48,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', '', 'Two', 'Three'
             $result = @(Get-StringToken -String $line)
 
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Treats two consecutive quotation marks inside a quoted string as an escaped quotation mark' {
@@ -54,20 +56,15 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One"Two', 'Three'
             $result = @(Get-StringToken -String $line)
 
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Does not treat quotation marks inside a non-quoted token as anything special' {
             $line = 'One"Two"Three'
-            $expected = 'One"Two"Three'
+            $expected = @('One"Two"Three')
             $result = @(Get-StringToken -String $line)
 
-            $result.Count | Should Be (1)
-            $result[0] | Should BeExactly $expected
+            Assert-ArraysAreEqual $result $expected
         }
         
         It 'Does not allow multi-line quoted tokens' {
@@ -75,11 +72,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', 'Two"'
             $result = @(Get-StringToken -String $lines)
 
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Does not use any escape characters other than a double quotation mark' {
@@ -93,10 +86,10 @@ Describe 'Get-StringToken (public API)' {
             {
                 if ($charsToIgnore -contains $char) { continue }
 
-                $string = "`"$char`" `""
+                $string = "`"$char`""
                 $result = @(Get-StringToken -String $string)
 
-                $result[0] | Should Be $char
+                Assert-ArraysAreEqual $result $char
             }
         }
 
@@ -105,11 +98,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'This is a test', 'SecondToken'
             $result = @(Get-StringToken -String $string)
 
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
     }
 
@@ -130,14 +119,12 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'Begin' + '"' * $escapeChars.Count + 'End'
 
             $result = @(Get-StringToken -String $string -Escape $escapeChars)
-            $result.Count | Should Be (1)
-            $result | Should Be $expected
+            Assert-ArraysAreEqual $result $expected
 
             $escapeCharsAsString = -join $escapeChars
 
             $result = @(Get-StringToken -String $string -Escape $escapeCharsAsString)
-            $result.Count | Should Be (1)
-            $result | Should Be $expected
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Does not treat escape characters as anything special if they are not followed by a qualifier' {
@@ -147,8 +134,7 @@ Describe 'Get-StringToken (public API)' {
 
             $result = @(Get-StringToken -String $string -Escape $escapeChar)
 
-            $result.Count | Should Be (1)
-            $result | Should Be $expected
+            Assert-ArraysAreEqual $result $expected
         }
     }
 
@@ -159,19 +145,11 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', 'Two', 'Three', 'Four'
 
             $result = @(Get-StringToken -String $string -Delimiter $delimiters)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
 
             $delimitersAsString = -join $delimiters
             $result = @(Get-StringToken -String $string -Delimiter $delimitersAsString)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'No longer treats the default delimiters of tab / space as delimiters if the user explicitly specifies them' {
@@ -180,11 +158,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = "One Two`tThree", 'Four'
 
             $result = @(Get-StringToken -String $string -Delimiter $delimiter)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Still produces empty tokens between consecutive delimiters when explicitly specified' {
@@ -193,11 +167,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', '', 'Two'
 
             $result = @(Get-StringToken -String $string -Delimiter $delimiter)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
     }
 
@@ -208,19 +178,11 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One One', 'Two Two', 'Three Three'
 
             $result = @(Get-StringToken -String $string -Qualifier $qualifiers)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
 
             $qualifiersAsString = -join $qualifiers
             $result = @(Get-StringToken -String $string -Qualifier $qualifiersAsString)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Only uses matching qualifiers to enclose a single token; other qualifiers are treated as literals within that token' {
@@ -229,11 +191,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One | One', 'Two # Two'
 
             $result = @(Get-StringToken -String $string -Qualifier $qualifiers)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'No longer treats the default qualifer (double quotation mark) as a qualifier when the user explicitly specifies others' {
@@ -242,11 +200,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = '"One', 'Two"'
 
             $result = @(Get-StringToken -String $string -Qualifier $qualifiers)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
     }
 
@@ -256,11 +210,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', 'Two'
 
             $result = @(Get-StringToken -String $string -NoDoubleQualifier)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
     }
 
@@ -270,11 +220,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = "One`r`nTwo", 'Three'
 
             $result = @(Get-StringToken -String $string -Span)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Allows quoted tokens to span multiple lines across input strings, using a default separator of CRLF' {
@@ -282,11 +228,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = "One`r`nTwo", 'Three'
 
             $result = @($strings | Get-StringToken -Span)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Allows quoted tokens to span multiple lines across input strings, using a user-specified line separator' {
@@ -295,11 +237,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = "One|Two", 'Three'
 
             $result = @($strings | Get-StringToken -Span -LineDelimiter $lineDelimiter)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
 
         It 'Only uses the -LineDelimiter value when spanning lines across input string boundaries, not embedded EOL chars' {
@@ -308,11 +246,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = "One`r`nTwo", 'Three'
 
             $result = @($string | Get-StringToken -Span -LineDelimiter $lineDelimiter)
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
     }
 
@@ -322,11 +256,7 @@ Describe 'Get-StringToken (public API)' {
             $expected = 'One', 'Two'
             $result = @(Get-StringToken -String $string -IgnoreConsecutiveDelimiters)
 
-            $result.Count | Should Be $expected.Count
-            for ($i = 0; $i -lt $result.Count; $i++)
-            {
-                $result[$i] | Should BeExactly $expected[$i]
-            }
+            Assert-ArraysAreEqual $result $expected
         }
     }
 }
